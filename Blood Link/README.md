3f0d78f1c5b4a11251ede0f1bcce8e6b5c203523
Nandan Nayak <nandannayak2102@gmail.com>
2025-12-23 19:32:00 +0530

    commit (CRUD with EJS & MongoDB)

---
+++ Blood Link/README.md
@@ -0,0 +1,4 @@
+# 🩸 Blood Link
+
+Blood Link is a blood donation management project built using EJS, Node.js, Express, and MongoDB.  
+It performs complete CRUD operations on donor data, allowing users to view, update, and delete donors easily by their ID.
--- Blood Link/index.js
+++ Blood Link/index.js
@@ -70,3 +70,10 @@ app.post('/grouped',async(req,res)=>{
         res.render('finalgrp.ejs',{allDonors})
     }
 })
+
+app.delete('/delete/:id',async(req,res)=>{
+    const id=req.params.id
+    console.log(id)
+    await Donation.findByIdAndDelete(id)
+    res.redirect("/list")
+})
