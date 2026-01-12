0537ac461eb6ed1d705e94e6d53b30d2cc03ec1e
Nandan Nayak <nandannayak2102@gmail.com>
2026-01-12 17:21:56 +0530

    Added Error Handling using middlewares

--- ChatCards/index.js
+++ ChatCards/index.js
@@ -3,7 +3,7 @@ const app=express()
 const mongoose=require("mongoose")
 const path=require("path")
 const Chat=require("./models/chat")
-
+const ExpressError=require("./ExpressError")
 const methodOverride=require("method-override")
 app.use(methodOverride("_method"))
 
@@ -24,17 +24,25 @@ async function main() {
 
 //Index route
 app.get('/chats',async(req,res)=>{
-  let chats=await Chat.find();
+  try{
+    let chats=await Chat.find();
   console.log(chats.length)
   res.render("index",{chats:chats})
+  }
+  catch(e){
+    next(e);
+  }
+  
 })
 //New chat
 app.get("/chats/new",(req,res)=>{
+  // throw new ExpressError(404,"Page Not Found")
   res.render("new.ejs")
 })
 
-app.post("/chats",(req,res)=>{
-  let from=req.body.from;
+app.post("/chats",async(req,res,next)=>{
+  try{
+    let from=req.body.from;
   let to=req.body.to;
   let msg=req.body.msg;
   let newChat=new Chat({
@@ -43,26 +51,57 @@ app.post("/chats",(req,res)=>{
     msg:msg,
     created_at:new Date()
   })
-  newChat.save();
+  await newChat.save();
   res.redirect("/chats")
+  }
+  catch(e){
+    next(e);
+  }
+  
   
 })
+//********************/
 //Update
-app.get("/update/:id",async(req,res)=>{
+app.get("/update/:id",async(req,res,next)=>{
+  try{
+    let id=req.params.id;
   let chats=await Chat.findById( req.params.id);
+  if(!chats){
+    next(new ExpressError(404,"Chat Not Found"))
+  } 
   res.render("update.ejs",{id:req.params.id,from:chats.from,to:chats.to,msg:chats.msg})
+  }
+  catch(e){
+    next(e);
+  }
 })
 //Patch
 app.patch("/update/:id",async(req,res)=>{
-  let id=req.params.id;
+  try{
+    let id=req.params.id;
   let newmsg=req.body.msg;
   await Chat.findByIdAndUpdate(id,{$set:{msg:newmsg}});
   res.redirect("/chats")
+  }
+  catch(e){
+    next(e);
+  }
   
 })
 //Delete
 app.delete("/chats/:id",async(req,res)=>{
-  let id=req.params.id;
+  try{
+    let id=req.params.id;
   await Chat.findByIdAndDelete(id);
   res.redirect("/chats")
+  }
+  catch(e){ 
+    next(e);
+  }
 })
+app.use((err,req,res,next)=>{
+  let {status=500,message="Something went wrong"}=err;
+  res.status(status).send(message);
+})
+//Error handling middleware
+
